a9a00973e9d3e9a3ac09b0febe53acfe9d61bc4a
yjtlvkyx <756994208@qq.com>
2019-02-18 15:17:38 +0800

    这是第一次提交

---
+++ product/src/day6/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+    render() {
+        return (
+            <div>
+                这个能上来么
+            </div>
+        );
+    }
+}
+
+export default componentName;
