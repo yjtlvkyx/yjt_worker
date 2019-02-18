2b108f72494411fb456e67398408d0c89d5ab186
yjtlvkyx <756994208@qq.com>
2019-02-18 15:24:37 +0800

    这是第二次提交

---
+++ product/src/day7/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class componentName extends Component {
+    render() {
+        return (
+            <div>
+                这是第二次提交
+            </div>
+        );
+    }
+}
+
+export default componentName;
